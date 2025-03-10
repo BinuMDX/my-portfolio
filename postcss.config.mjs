d7cffc602b51cdd6518c93893cac67b4ec87450a
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-03-10 09:48:47 +0530

    feat: created navbar

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,20 @@
+import { Outfit, Ovo } from "next/font/google";
+
 const config = {
+  theme: {
+    extend: {
+      colors: {
+        lightHover: "#fcf4ff", 
+        darkHover: "#2a004a",
+        darkTheme: "#11001F",
+      },
+      FontFamily: {
+        Outfit  : ["Outfit", "sans-serif"],
+        Ovo     : ["Ovo", "serif"],
+      },
+    },
+  },
+
   plugins: ["@tailwindcss/postcss"],
 };
 
