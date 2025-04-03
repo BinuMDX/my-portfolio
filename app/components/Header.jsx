a1ca854870c34a94b671dd344375fe08ff84a35a
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-04-03 22:01:17 +0530

    dark mode

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
         <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'> Software Engineer</h1>
         <p className='max-w-2xl mx-auto font-Ovo'>I'm a passionate bla bla...</p>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <a href='#contact' className='flex items-center gap-2 px-10 py-3 border-white border-gray-500 rounded-full ml-4 bg-black text-white font-Ovo'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
-            <a href='/sample-resume.pdf' download className='flex items-center gap-2 px-10 py-3 border border-gray-500 rounded-full ml-4 font-Ovo'>My Resume<Image src={assets.download_icon} alt='' className='w-4'/></a>
+            <a href='#contact' className='flex items-center gap-2 px-10 py-3 border border-gray-500 rounded-full ml-4 bg-black text-white font-Ovo dark:bg-transparent'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
+            <a href='/sample-resume.pdf' download className='flex items-center gap-2 px-10 py-3 border border-gray-500 rounded-full ml-4 font-Ovo bg-white dark:text-black'>My Resume<Image src={assets.download_icon} alt='' className='w-4'/></a>
         </div>
        </div>
     );
